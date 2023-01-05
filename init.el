09840b8cc98fa197da78c0a718bb18784163f401
Jack8472 <3611452+Jack8472@users.noreply.github.com>
2023-01-05 06:51:28 +0100

    debug init.el, yas bindigns

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
+;; (org-babel-load-file
+;;  (expand-file-name
+;;   "config.org"
+;;   user-emacs-directory))
+(org-babel-load-file "~/.emacs.d/config.org")
 
