6011329f6c7784e6d2aaf800a34e9396954c8013
Jack8472 <3611452+Jack8472@users.noreply.github.com>
2023-01-06 15:53:29 +0100

    bugfixes

---
+++ early-init.el
@@ -0,0 +1,5 @@
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold most-positive-fixnum)
+;; Loads a nice blue theme, avoids the white screen flash on startup.
+(load-theme 'modus-vivendi t)
+
