72e87d6c7716ab18eca841e015740367edd0e37a
Jack8472 <3611452+Jack8472@users.noreply.github.com>
2023-01-22 15:07:41 +0100

    update

---
+++ elpa/consult-20230121.1135/consult-pkg.el
@@ -0,0 +1,11 @@
+(define-package "consult" "20230121.1135" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.1.1"))
+  :commit "4a7167c331d3c0c0d60b1d2d54c840f33e446683" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
