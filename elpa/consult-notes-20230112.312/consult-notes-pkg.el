ddf5749ae9d0861f5a849f0d29b381d2cef0efca
Jack8472 <3611452+Jack8472@users.noreply.github.com>
2023-02-24 11:07:42 +0100

    consult-notes

---
+++ elpa/consult-notes-20230112.312/consult-notes-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult-notes" "20230112.312" "Manage notes with consult"
+  '((emacs "27.1")
+    (consult "0.17")
+    (s "1.12.0")
+    (dash "2.19"))
+  :commit "4d92996f676cecfebea6519dadf8cf185621a18a" :authors
+  '(("Colin McLear" . "mclear@fastmail.com"))
+  :maintainer
+  '("Colin McLear")
+  :keywords
+  '("convenience")
+  :url "https://github.com/mclear-tools/consult-notes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
