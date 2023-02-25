27f5d543afd9d80caeaed2f9051f871023a73972
Jack8472 <3611452+Jack8472@users.noreply.github.com>
2023-02-25 12:24:26 +0100

    local org agenda leader finally sort-of fixed

--- elpa/marginalia-20230206.2022/marginalia-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from marginalia.el  -*- no-byte-compile: t -*-
-(define-package "marginalia" "20230206.2022" "Enrich existing commands with completion annotations" '((emacs "27.1") (compat "29.1.3.0")) :commit "46cd60a1e010eda57c05a585e8707f9d146f96fb" :authors '(("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/marginalia")
--- elpa/org-modern-20230215.718/org-modern-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from org-modern.el  -*- no-byte-compile: t -*-
-(define-package "org-modern" "20230215.718" "Modern looks for Org" '((emacs "27.1")) :commit "42fdbd2d27f5a8652593577a200eeea96555d018" :authors '(("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/org-modern")
