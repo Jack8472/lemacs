db28e6d8c14e6b2d61f98c44337bec1a4873c7b3
Jack8472 <3611452+Jack8472@users.noreply.github.com>
2023-03-01 08:05:50 +0100

    fixes

---
+++ elpa/embark-20230228.2035/embark-pkg.el
@@ -0,0 +1,13 @@
+(define-package "embark" "20230228.2035" "Conveniently act on minibuffer completions"
+  '((emacs "27.1")
+    (compat "29.1.3.4"))
+  :commit "fda00ffac57238fbc4da12ad8c5cb1e08b19544f" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("convenience")
+  :url "https://github.com/oantolin/embark")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
